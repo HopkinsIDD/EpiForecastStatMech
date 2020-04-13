a820ed4443b01f3444cceac5e3a95d1e79d3bf9d
Stephan Hoyer <shoyer@google.com>
2020-04-13 13:14:58 -0700

    Example of a broken test
    How does this look?

--- tests/test_sir_sim.py
+++ tests/test_sir_sim.py
@@ -15,3 +15,6 @@ class GenerateSirSimulations(absltest.TestCase):
         (num_epidemics, num_important_cov, num_unimportant_cov),
         num_simulations, num_epidemics, constant_pop_size=10000)
     assert len(trajectories) == num_epidemics
+
+  def test_broken(self):
+    assert False, "what happens here?"
