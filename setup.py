571632fcfd681d63d52e81812ee2520febc96112
Stephan Hoyer <shoyer@google.com>
2020-04-13 19:15:15 -0700

    reformat requirements

--- setup.py
+++ setup.py
@@ -4,5 +4,16 @@ setuptools.setup(
     name="EpiForecastStatMech",
     version="0.1",
     packages=setuptools.find_packages(),
-    install_requires=open("requirements.txt").read(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "tensorflow",
+        "tensorflow_probability",
+        "jax",
+        "jaxlib",
+        "glmnet_py",
+    ],
 )
