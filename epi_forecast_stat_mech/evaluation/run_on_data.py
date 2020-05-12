c48aec6048ca262a5e79ebadc149627831a632f3
geraschenko <geraschenko@google.com>
2020-05-11 18:11:15 -0700

    Google export
    PiperOrigin-RevId: 311034471

--- epi_forecast_stat_mech/evaluation/run_on_data.py
+++ epi_forecast_stat_mech/evaluation/run_on_data.py
@@ -1,6 +1,8 @@
 # Lint as: python3
 """Metrics for evaluating models."""
 
+import json
+import numpy as np
 import xarray as xr
 
 
@@ -16,18 +18,17 @@ def train_test_split_time(data, split_day):
     test_data: an xr.Dataset containing the testing data for a model.
   """
   # everything before split_day
-  train_data = data.sel(time=slice(None, split_day - 1))
+  train_data = data.sel(time=slice(None, split_day - 1)).copy()
   # everything after split_day
-  test_data = data.sel(time=slice(split_day, None))
+  test_data = data.sel(time=slice(split_day, None)).copy()
   # drop all variables that won't exist on real test data
   test_covariates = test_data[['location', 'time', 'static_covariate']]
   test_covariates['static_covariates'] = data['static_covariates']
 
-  split_spec = ('train_test_split_time', dict(split_day=split_day))
   train_data.attrs['split'] = 'train'
-  train_data.attrs['split_spec'] = split_spec
+  train_data.attrs['split_spec'] = 'train_test_split_time'
   test_covariates.attrs['split'] = 'test'
-  test_covariates.attrs['split_spec'] = split_spec
+  test_covariates.attrs['split_spec'] = 'train_test_split_time'
 
   return train_data, test_covariates
 
@@ -43,6 +44,6 @@ def shard_locations_randomly(data, num_shards=5, seed=0):
   result = [data.sel(location=locs).copy() for locs in location_shards]
   for i, shard in enumerate(result):
     shard.attrs['shard'] = i
-    shard.attrs['shard_spec'] = ('shard_locations_randomly',
-                                 dict(num_shards=num_shards, seed=seed))
+    shard.attrs['shard_spec'] = json.dumps(
+        ('shard_locations_randomly', dict(num_shards=num_shards, seed=seed)))
   return result
